b1388be748f08cb161918ef09dfd7bf4a67800eb
Andrei Amaral <andrei.amaral@redfox.tech>
2022-02-11 23:22:20 -0300

    adding routes

--- src/presentation/screens/Welcome/index.tsx
+++ src/presentation/screens/Welcome/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Text } from 'react-native';
 import * as S from './styles';
 
 const Welcome = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <Text> Welcome screen</Text>
+    </S.Container>
+  );
 };
 
 export default Welcome;
