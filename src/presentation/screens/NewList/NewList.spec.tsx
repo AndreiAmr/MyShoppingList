87ce0ef838c59b78f234474375e56ee5f1737f7d
Andrei Soneca <andreiamaral74@gmail.com>
2022-02-27 15:12:17 -0300

    implementing total screen

--- src/presentation/screens/NewList/NewList.spec.tsx
+++ src/presentation/screens/NewList/NewList.spec.tsx
@@ -30,7 +30,11 @@ jest.mock('@fortawesome/react-native-fontawesome', () => {
   };
 });
 
-jest.mock('react-native-gesture-handler', () => {});
+jest.mock('react-native-gesture-handler', () => {
+  return {
+    ScrollView: '',
+  };
+});
 
 jest.mock('react-native-reanimated', () => {
   return {
