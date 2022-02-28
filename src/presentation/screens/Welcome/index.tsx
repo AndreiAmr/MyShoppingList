66f6a24333b96a99f3d04ba99d0a51d4c77234fd
Andrei Soneca <andreiamaral74@gmail.com>
2022-02-28 13:08:05 -0300

    adding auto login

--- src/presentation/screens/Welcome/index.tsx
+++ src/presentation/screens/Welcome/index.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import * as S from './styles';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import constants from '../../routes/constants';
 import { useTheme } from 'styled-components/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import keys from '../../storage/keys';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const Welcome = () => {
   const theme = useTheme();
   const navigation = useNavigation();
+  const [loading, setLoading] = useState<boolean>(false);
+
   const handleSigninGoogle = async () => {
     try {
       const { idToken } = await GoogleSignin.signIn();
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      await auth().signInWithCredential(googleCredential);
+      const {
+        user: { uid },
+      } = await auth().signInWithCredential(googleCredential);
+      await AsyncStorage.setItem(keys.userId, uid);
       navigation.navigate(constants.home);
     } catch (err) {
       return err;
     }
   };
+
+  const getUserTokenOnStorage = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const userID = await AsyncStorage.getItem(keys.userId);
+      if (userID !== null) {
+        navigation.navigate(constants.home);
+        setLoading(false);
+      }
+    } catch (err) {
+      setLoading(false);
+    }
+  }, [navigation]);
+
+  useEffect(() => {
+    getUserTokenOnStorage();
+  }, [getUserTokenOnStorage]);
 
   return (
     <S.Container>
@@ -34,16 +60,22 @@ const Welcome = () => {
         Sua lista de compras inteligente!
       </S.Description>
 
-      <S.Button
-        testID="google-button-test-id"
-        bottomLeft
-        onPress={handleSigninGoogle}
-      >
-        <S.ButtonText>Entrar com Google</S.ButtonText>
-      </S.Button>
-      <S.Button testID="about-button-test-id" topLeft>
-        <S.ButtonText>Sobre o app</S.ButtonText>
-      </S.Button>
+      {loading ? (
+        <ActivityIndicator color={theme.color.primary} size={RFValue(40)} />
+      ) : (
+        <>
+          <S.Button
+            testID="google-button-test-id"
+            bottomLeft
+            onPress={handleSigninGoogle}
+          >
+            <S.ButtonText>Entrar com Google</S.ButtonText>
+          </S.Button>
+          <S.Button testID="about-button-test-id" topLeft>
+            <S.ButtonText>Sobre o app</S.ButtonText>
+          </S.Button>
+        </>
+      )}
     </S.Container>
   );
 };
